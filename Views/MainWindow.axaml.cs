3c40b7e15475cf6bff744c9965acb93cb0c1da32
CornyVanBeck <davidgarus@gmail.com>
2022-04-09 21:24:34 +0200

    Initial Commit

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace kitchenview.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
